23e2d2b3864fcd5b5e29a297bdff44a2aac924aa
Jannis Toumanidis <jtoumani@3-F-9.42heilbronn.de>
2025-11-07 13:45:47 +0100

    deletet some tester files

--- src/parsing/lexing/gc_substr_shim.c
+++
@@ -1,9 +0,0 @@
-#include "libft/libft.h"
-
-/* Minimaler Wrapper für parse_test */
-char	*gc_substr(char *s, unsigned int start, size_t len)
-{
-    if (!s)
-        return (NULL);
-    return (ft_substr((const char *)s, start, len));
-}
